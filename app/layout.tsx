aecfdaff3e16f58ee1ea7bafd691992882657986
Evan Alvarez <evanfalvarez@tutamail.com>
2024-08-31 22:59:56 -0500

    removed custom cursor, added about me section (plus an image)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "@/components/NavigationBar";
-import AnimatedCursor from "react-animated-cursor";
 
 const dmsans = DM_Sans({ subsets: ["latin"] });
 
@@ -20,27 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={dmsans.className}>
         <NavigationBar />
-        <AnimatedCursor
-          innerSize={10}
-          outerSize={10}
-          color="88, 129, 87"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={4}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-          ]}
-        />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { ArrowDown } from "lucide-react";
-import { useRef } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  const aboutRef = useRef<HTMLDivElement>(null);
-
   const handleScrollClick = () => {
-    aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+    document.getElementById("about")?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -17,7 +15,7 @@ export default function Home() {
           Evan Alvarez
         </p>
         <p className="text-xl sm:text-3xl font-semibold text-fern">
-          I like to code.
+          Programmer. Designer. Student.
         </p>
         <button
           onClick={handleScrollClick}
@@ -28,9 +26,38 @@ export default function Home() {
         </button>
       </div>
       <div
-        ref={aboutRef}
-        className="h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col items-center justify-center relative"
-      ></div>
+        id="about"
+        className="min-h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col md:flex-row items-center justify-center relative"
+      >
+        <div className="w-full md:w-1/2 h-full flex items-center justify-end p-8 md:pr-4">
+          <div className="relative w-4/5 aspect-square">
+            <Image
+              src="/evan_image_01.jpg"
+              alt="Evan Alvarez"
+              height={764}
+              width={600}
+              className="object-cover rounded-lg border-2 shadow-xl"
+            />
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 p-8 md:pl-4">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <h2 className="text-4xl font-bold text-fern mb-4">About Me</h2>
+            <p className="text-lg text-hunter">
+              Hey! I'm Evan. I am a student at Westwood High School with a deep
+              passion for programming. What I love most about programming is the
+              creativity and uniqueness involved. Like a cryptographic hash, no
+              two peoples solution to a complex problem will be the same. If you
+              couldn't tell by that last sentence, I also take a strong interest
+              in security and cryptography. On a normal day, you might find me
+              working on a new project, contributing to open source, or even
+              reading philosophy. I'm always looking to connect with others who
+              share my enthusiasm for programming and who will help me grow both
+              personally and professionally.
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
