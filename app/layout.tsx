4c56c7bb2346c03a860a0c3aac1a21a93d81715c
Evan Alvarez <evanfalvarez@tutamail.com>
2024-08-31 21:58:09 -0500

    changed up the cursor and added scroll down button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "@/components/NavigationBar";
+import AnimatedCursor from "react-animated-cursor";
 
 const dmsans = DM_Sans({ subsets: ["latin"] });
 
@@ -19,6 +20,27 @@ export default function RootLayout({
     <html lang="en">
       <body className={dmsans.className}>
         <NavigationBar />
+        <AnimatedCursor
+          innerSize={10}
+          outerSize={10}
+          color="88, 129, 87"
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={4}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+          ]}
+        />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,36 @@
+"use client";
+
 import { ArrowDown } from "lucide-react";
+import { useRef } from "react";
 
 export default function Home() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+
+  const handleScrollClick = () => {
+    aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <main>
-      <div className="h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col items-center justify-center">
-        <p className="text-5xl sm:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-sage to-fern py-6">
+      <div className="h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col items-center justify-center relative">
+        <p className="text-6xl sm:text-[11rem] font-bold relative bg-clip-text text-transparent bg-gradient-to-b from-sage to-fern py-6 text-left">
           Evan Alvarez
         </p>
-        <p className="text-xl sm:text-3xl font-semibold text-brunswick">
+        <p className="text-xl sm:text-3xl font-semibold text-fern">
           I like to code.
         </p>
+        <button
+          onClick={handleScrollClick}
+          className="absolute bottom-8 bg-white border-fern border-2 text-fern hover:bg-fern hover:text-white rounded-full p-4 transition-colors duration-300"
+          aria-label="Scroll to next section"
+        >
+          <ArrowDown size={24} />
+        </button>
       </div>
+      <div
+        ref={aboutRef}
+        className="h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col items-center justify-center relative"
+      ></div>
     </main>
   );
 }
