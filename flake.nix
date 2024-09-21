804dec1d36c975d66abc87a8f03411d4d91a58b0
Evan Alvarez <evanfalvarez@tutamail.com>
2024-09-21 15:45:36 -0500

    added flake.nix (for ease of use with nix)

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix flake that contains all of the requierments for running my personal website";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+	    bun
+          ];
+        };
+      });
+    };
+}
