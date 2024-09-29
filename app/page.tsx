527f590601c02ccf49936eb3ec6c47cf8c088300
Evan Alvarez <evanfalvarez@tutamail.com>
2024-09-29 17:58:56 -0500

    added skills button and fixed smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { ArrowDown } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
-  const handleScrollToAbout = () => {
-    document.getElementById("about")?.scrollIntoView({ behavior: "smooth" });
+  const handleSmoothScroll = (targetId: string) => {
+    document.getElementById(targetId)?.scrollIntoView({ behavior: "smooth" });
   };
 
   const skills = [
@@ -19,107 +19,107 @@ export default function Home() {
   ];
 
   return (
-    <main>
-      <div className="h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col items-center justify-center relative">
-        <p className="text-6xl sm:text-[11rem] text-center font-bold relative bg-clip-text text-transparent bg-gradient-to-b from-sage to-fern py-6">
-          Evan Alvarez
-        </p>
-        <p className="text-xl sm:text-3xl font-semibold text-fern">
-          Programmer. Designer. Student.
-        </p>
-        <button
-          onClick={handleScrollToAbout}
-          className="absolute bottom-8 bg-white border-fern border-2 text-fern hover:bg-fern hover:text-white rounded-full p-4 transition-colors duration-300"
-          aria-label="Scroll to about section"
-        >
-          <ArrowDown size={24} />
-        </button>
-      </div>
+      <main>
+        <div className="h-screen w-full bg-white bg-dot-hunter/[0.4] flex flex-col items-center justify-center relative">
+          <p className="text-6xl sm:text-[11rem] text-center font-bold relative bg-clip-text text-transparent bg-gradient-to-b from-sage to-fern py-6">
+            Evan Alvarez
+          </p>
+          <p className="text-xl sm:text-3xl font-semibold text-fern">
+            Programmer. Designer. Student.
+          </p>
+          <button
+              onClick={() => handleSmoothScroll("about")}
+              className="absolute bottom-8 bg-white border-fern border-2 text-fern hover:bg-fern hover:text-white rounded-full p-4 transition-colors duration-300"
+              aria-label="Scroll to about section"
+          >
+            <ArrowDown size={24} />
+          </button>
+        </div>
 
-      <div id="about" className="w-full bg-white bg-dot-hunter/[0.4] py-16">
-        <div className="container mx-auto flex flex-col md:flex-row items-center justify-center">
-          <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-8">
-            <div className="relative w-full aspect-square max-w-md mx-auto px-4 sm:px-0">
-              <Image
-                src="/evan_image_01.jpg"
-                alt="Evan Alvarez"
-                height={764}
-                width={600}
-                className="rounded-lg border-2 shadow-xl"
-              />
+        <div id="about" className="w-full bg-white bg-dot-hunter/[0.4] py-16">
+          <div className="container mx-auto flex flex-col md:flex-row items-center justify-center">
+            <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-8">
+              <div className="relative w-full aspect-square max-w-md mx-auto px-4 sm:px-0">
+                <Image
+                    src="/evan_image_01.jpg"
+                    alt="Evan Alvarez"
+                    height={764}
+                    width={600}
+                    className="rounded-lg border-2 shadow-xl"
+                />
+              </div>
             </div>
-          </div>
-          <div className="w-full md:w-1/2">
-            <div className="bg-white p-8 rounded-lg shadow-lg">
-              <h2 className="text-4xl font-bold text-fern mb-4">About Me</h2>
-              <p className="text-lg text-hunter">
-                Hey! I&apos;m Evan. I am a student at Westwood High School with
-                a deep passion for programming. What I love most about
-                programming is the creativity and uniqueness involved. Like a
-                cryptographic hash, no two peoples solution to a complex problem
-                will be the same. If you couldn&apos;t tell by that last
-                sentence, I also take a strong interest in security and
-                cryptography. On a normal day, you might find me working on a
-                new project, contributing to open source, or even reading
-                philosophy. I&apos;m always looking to connect with others who
-                share my enthusiasm for programming and who will help me grow
-                both personally and professionally.
-              </p>
+            <div className="w-full md:w-1/2">
+              <div className="bg-white p-8 rounded-lg shadow-lg">
+                <h2 className="text-4xl font-bold text-fern mb-4">About Me</h2>
+                <p className="text-lg text-hunter">
+                  Hey! I&apos;m Evan. I am a student at Westwood High School with
+                  a deep passion for programming. What I love most about
+                  programming is the creativity and uniqueness involved. Like a
+                  cryptographic hash, no two peoples solution to a complex problem
+                  will be the same. If you couldn&apos;t tell by that last
+                  sentence, I also take a strong interest in security and
+                  cryptography. On a normal day, you might find me working on a
+                  new project, contributing to open source, or even reading
+                  philosophy. I&apos;m always looking to connect with others who
+                  share my enthusiasm for programming and who will help me grow
+                  both personally and professionally.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div id="skills" className="w-full bg-white bg-dot-hunter/[0.4] py-16">
-        <div className="container mx-auto flex flex-col md:flex-row items-start justify-center">
-          <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-8">
-            <div className="bg-white p-8 rounded-lg shadow-lg">
-              <h2 className="text-4xl font-bold text-fern mb-4">Skills</h2>
-              <p className="text-lg text-hunter mb-4">
-                Over the 5 years i&apos;ve been programming, these are some of
-                the skills i&apos;ve learned. Mainly, I do web development,
-                because I like the design aspect that goes along with it. In the
-                past, i&apos;ve also written code in Python, Java, C, and C++,
-                though I would not say these are my forte.
-              </p>
+        <div id="skills" className="w-full bg-white bg-dot-hunter/[0.4] py-16 relative">
+          <div className="container mx-auto flex flex-col md:flex-row items-start justify-center">
+            <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-8">
+              <div className="bg-white p-8 rounded-lg shadow-lg">
+                <h2 className="text-4xl font-bold text-fern mb-4">Skills</h2>
+                <p className="text-lg text-hunter mb-4">
+                  Over the 5 years i&apos;ve been programming, these are some of
+                  the skills i&apos;ve learned. Mainly, I do web development,
+                  because I like the design aspect that goes along with it. In the
+                  past, i&apos;ve also written code in Python, Java, C, and C++,
+                  though I would not say these are my forte.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="w-full md:w-1/2">
-            <div className="bg-white p-8 rounded-lg shadow-lg">
-              <div className="flex flex-col md:flex-row items-center justify-between">
-                <ul className="space-y-4 mb-8 md:mb-0">
-                  {skills.map((skill, index) => (
-                    <li key={index} className="flex items-center space-x-4">
-                      <Image
-                        src={skill.icon}
-                        alt={skill.name}
-                        width={32}
-                        height={32}
-                      />
-                      <span className="text-lg font-semibold text-hunter">
+            <div className="w-full md:w-1/2">
+              <div className="bg-white p-8 rounded-lg shadow-lg">
+                <div className="flex flex-col md:flex-row items-center justify-between">
+                  <ul className="space-y-4 mb-8 md:mb-0">
+                    {skills.map((skill, index) => (
+                        <li key={index} className="flex items-center space-x-4">
+                          <Image
+                              src={skill.icon}
+                              alt={skill.name}
+                              width={32}
+                              height={32}
+                          />
+                          <span className="text-lg font-semibold text-hunter">
                         {skill.name}
                       </span>
-                    </li>
-                  ))}
-                </ul>
-                <div className="flex justify-center md:justify-end">
-                  <a
-                    href="https://github.com/sheikhevan/sheikhevan"
-                    className="inline-block"
-                  >
-                    <Image
-                      alt="github.com/sheikhevan"
-                      height={200}
-                      width={400}
-                      src="https://github-readme-stats.vercel.app/api/top-langs?username=sheikhevan&langs_count=8&theme=transparent&layout=donut-vertical&card_width=320"
-                    />
-                  </a>
+                        </li>
+                    ))}
+                  </ul>
+                  <div className="flex justify-center md:justify-end">
+                    <a
+                        href="https://github.com/sheikhevan/sheikhevan"
+                        className="inline-block"
+                    >
+                      <Image
+                          alt="github.com/sheikhevan"
+                          height={200}
+                          width={400}
+                          src="https://github-readme-stats.vercel.app/api/top-langs?username=sheikhevan&langs_count=8&theme=transparent&layout=donut-vertical&card_width=320"
+                      />
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 }
--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -16,12 +16,12 @@ interface NavLinkProps {
 }
 
 const NavLink: React.FC<NavLinkProps> = ({
-  href,
-  icon,
-  children,
-  onClick,
-  smoothScroll,
-}) => {
+                                           href,
+                                           icon,
+                                           children,
+                                           onClick,
+                                           smoothScroll,
+                                         }) => {
   const pathname = usePathname();
   const isActive = pathname === href;
 
@@ -46,16 +46,16 @@ const NavLink: React.FC<NavLinkProps> = ({
   };
 
   return (
-    <Link href={href} passHref>
-      <Button
-        variant={isActive ? "secondary" : "ghost"}
-        className="w-full justify-start"
-        onClick={handleClick}
-      >
-        {icon}
-        <span className="ml-2">{children}</span>
-      </Button>
-    </Link>
+      <Link href={href} passHref>
+        <Button
+            variant={isActive ? "secondary" : "ghost"}
+            className="w-full justify-start"
+            onClick={handleClick}
+        >
+          {icon}
+          <span className="ml-2">{children}</span>
+        </Button>
+      </Link>
   );
 };
 
@@ -65,62 +65,70 @@ const NavigationBar: React.FC = () => {
   const closeSheet = () => setIsOpen(false);
 
   const NavContent: React.FC = () => (
-    <>
-      <NavLink
-        href="/"
-        icon={<Home className="h-4 w-4" />}
-        onClick={closeSheet}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        href="#about"
-        icon={<User className="h-4 w-4" />}
-        onClick={closeSheet}
-        smoothScroll
-      >
-        About
-      </NavLink>
-      <NavLink
-        href="/blog"
-        icon={<BookOpen className="h-4 w-4" />}
-        onClick={closeSheet}
-      >
-        Blog
-      </NavLink>
-    </>
+      <>
+        <NavLink
+            href="/"
+            icon={<Home className="h-4 w-4" />}
+            onClick={closeSheet}
+        >
+          Home
+        </NavLink>
+        <NavLink
+            href="#about"
+            icon={<User className="h-4 w-4" />}
+            onClick={closeSheet}
+            smoothScroll
+        >
+          About
+        </NavLink>
+        <NavLink
+            href="#skills"
+            icon={<BookOpen className="h-4 w-4" />}
+            onClick={closeSheet}
+            smoothScroll
+        >
+          Skills
+        </NavLink>
+        <NavLink
+            href="/blog"
+            icon={<BookOpen className="h-4 w-4" />}
+            onClick={closeSheet}
+        >
+          Blog
+        </NavLink>
+      </>
   );
 
   return (
-    <nav className="backdrop-blur border-b-2 border-gray-200 fixed top-0 left-0 right-0 z-50">
-      <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-        <Link href="/" passHref>
-          <Button
-            variant="ghost"
-            className="text-3xl hover:bg-white hover:text-hunter text-hunter font-bold"
-          >
-            EA
-          </Button>
-        </Link>
-        <div className="hidden md:flex items-center space-x-4">
-          <NavContent />
+      <nav className="backdrop-blur border-b-2 border-gray-200 fixed top-0 left-0 right-0 z-50">
+        <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+          <Link href="/" passHref>
+            <Button
+                variant="ghost"
+                className="text-3xl hover:bg-white hover:text-hunter text-hunter font-bold"
+            >
+              EA
+            </Button>
+          </Link>
+          <div className="hidden md:flex items-center space-x-4">
+            <NavContent />
+          </div>
+          <div className="flex items-center md:hidden">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Menu className="h-6 w-6" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="right" className="w-[200px] sm:w-[300px]">
+                <nav className="flex flex-col space-y-4 mt-4">
+                  <NavContent />
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </div>
         </div>
-        <div className="flex items-center md:hidden">
-          <Sheet open={isOpen} onOpenChange={setIsOpen}>
-            <SheetTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <Menu className="h-6 w-6" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="right" className="w-[200px] sm:w-[300px]">
-              <nav className="flex flex-col space-y-4 mt-4">
-                <NavContent />
-              </nav>
-            </SheetContent>
-          </Sheet>
-        </div>
-      </div>
-    </nav>
+      </nav>
   );
 };
 
